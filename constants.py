3dc85d07ef1b4052035b048016cea83d33147317
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-19 02:30:31 -0500

    UPDATING CONSTANTS

--- constants.py
+++ constants.py
@@ -48,3 +48,49 @@ RIGHT = "right"
 SPACE = "space"
 ENTER = "enter"
 PAUSE = "p"
+
+# SCENES
+NEW_GAME = 0
+TRY_AGAIN = 1
+NEXT_LEVEL = 2
+IN_PLAY = 3
+GAME_OVER = 4
+
+# LEVELS
+LEVEL_FILE = "batter/assets/data/level-{:03}.txt"
+BASE_LEVEL = 5
+
+
+##############################################
+##———————————SCRIPTING CONSTANTS————————————##
+##############################################
+
+# PHASES
+INITIALIZER = 0
+LOAD = 1
+INPUT = 2
+UPDATE = 3
+OUTPUT = 4
+UNLOAD = 5
+RELEASE = 6
+
+
+##############################################
+##————————————CASTING CONSTANTS—————————————##
+##############################################
+
+# STATS
+STATS_GROUP = "stats"
+DEFAULT_LIVES = 3
+MAXIMUM_LIVES = 5
+
+# HUD
+HUD_MARGIN = 15
+LEVEL_GROUP = "level"
+LIVES_GROUP = "lives"
+SCORE_GROUP = "score"
+LEVEL_FORMAT = "LEVEL: {}"
+LIVES_FORMAT = "LIVES: {}"
+SCORE_FORMAT = "SCORE: {}"
+
+# BALL
