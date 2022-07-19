706c5361d524f80893769f1465e4f332b61b61b6
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-19 02:21:08 -0500

    constants updating

--- constants.py
+++ constants.py
@@ -5,3 +5,46 @@ from game.casting.color import Color
 ##############################################
 ##——————————GENERAL GAME CONSTANTS——————————##
 ##############################################
+
+# GAME
+GAME_NAME = "Batter"
+FRAME_RATE = 60
+
+# SCREEN
+SCREEN_WIDTH = 1040
+SCREEN_HEIGHT = 680
+CENTER_X = SCREEN_WIDTH / 2
+CENTER_Y = SCREEN_HEIGHT / 2
+
+# FIELD
+FIELD_TOP = 60
+FIELD_BOTTOM = SCREEN_HEIGHT
+FIELD_LEFT = 0
+FIELD_RIGHT = SCREEN_WIDTH
+
+# FONT
+FONT_FILE = "batter/assets/fonts/zorque.otf"
+FONT_SMALL = 32
+FONT_LARGE = 48
+
+# SOUND
+BOUNCE_SOUND = "batter/assets/sounds/boing.wav"
+WELCOME_SOUND = "batter/assets/sounds/start.wav"
+OVER_SOUND = "batter/assets/sounds/over.wav"
+
+# TEXT
+ALIGN_CENTER = 0
+ALIGN_LEFT = 1
+ALIGN_RIGHT = 2
+
+# COLORS
+BLACK = Color(0, 0, 0)
+WHITE = Color(255, 255, 255)
+PURPLE = Color(255, 0, 255)
+
+# KEYS
+LEFT = "left"
+RIGHT = "right"
+SPACE = "space"
+ENTER = "enter"
+PAUSE = "p"
