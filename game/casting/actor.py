8887808c0f58b70b7675dbd8a947e605c5cf615d
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-19 22:29:37 -0500

    rectangle and point created and completed

--- game/casting/actor.py
+++ game/casting/actor.py
@@ -2,7 +2,8 @@ from game.casting.text import Text
 
 
 class Actor:
-    """A thing that participates in the game."""
+    """
+    A thing that participates in the game."""
 
     def __init__(self, debug = False):
         """Constructs a new Actor using the given group and id.
@@ -14,7 +15,8 @@ class Actor:
         self._debug = debug
 
     def is_debug(self):
-        """Whether or not the actor is being debugged.
+        """
+        Whether or not the actor is being debugged.
         
         Returns:
             True if the actor is being debugged; False if otherwise.
