55a1d6cf145f147fa73a0d214bbb380d81ece90e
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-21 17:34:11 -0500

    director updated.

---
+++ __main__.py
@@ -0,0 +1,2 @@
+from constants import *
+from game.directing.director import Director
--- game/directing/director.py
+++ game/directing/director.py
@@ -3,3 +3,57 @@ from game.casting.cast import Cast
 from game.directing.scene_manager import SceneManager
 from game.scripting.action_callback import ActionCallback
 from game.scripting.script import Script
+
+
+class Director(ActionCallback):
+    """
+    A person who directs the game.
+    """
+
+    def __init__(self, video_service):
+        """
+        Constructs a new Director using the specified video service.
+        
+        Args:
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._video_service = video_service
+        self._cast = Cast()
+        self._script = Script()
+        self._scene_manager = SceneManager()
+
+    def on_next(self, scene):
+        """
+        Overridden ActionCallback method transitions to next scene.
+
+        Args:
+            A number of representing the next scene to transition to.
+        """
+        self._scene_manager.prepare_scene(scene, self._cast, self._script)
+
+    def start_game(self):
+        """
+        Starts the game. Runs the main game loop.
+        """
+        self.on_next(NEW_GAME)
+        self._execute_actions(INITIALIZE)
+        self._execute_actions(LOAD)
+        while self._video_service.is_window_open():
+            self._execute_actions(INPUT)
+            self._execute_actions(UPDATE)
+            self._execute_actions(OUTPUT)
+        self._execute_actions(UNLOAD)
+        self._execute_actions(RELEASE)
+
+    def _execute_actions(self, group):
+        """
+        Calls execute for each actionin the given group.
+
+        Args:
+            group (String)  : The action group name.
+            cast (Cast)     : The cast of actors.
+            script (Script) : The script of actions.
+        """
+        actions = self._script.get_actions(group)
+        for action in actions:
+            action.execute(self._cast, self._script, self)
